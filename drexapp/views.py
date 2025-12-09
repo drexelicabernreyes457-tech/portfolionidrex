6c6c3faaaf0d30ea243b15507570126704329024
Drexelica Bern Reyes <drexelicabernreyes457@gmail.com>
2025-12-09 17:14:32 +0800

    Initial commit - add portfolio project

--- drexapp/views.py
+++ drexapp/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def portfolio(request):
-    return render(request, "portfolio.html")
+    return render(request, 'portfolio.html')
+
+def skills(request):
+    return render(request, 'skills.html')
+
+def about(request):
+    return render(request, 'about.html')
--- drexproj/urls.py
+++ drexproj/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
-from django.urls import path, include
+from django.urls import path
+from drexapp import views  # replace 'myapp' with your app name
 
 urlpatterns = [
-    path("", include("drexapp.urls")),
+    path('admin/', admin.site.urls),
+    path('', views.portfolio, name='portfolio'),
+    path('skills/', views.skills, name='skills'),
+    path('about/', views.about, name='about'),  # new URL for About Me
 ]
+
 
